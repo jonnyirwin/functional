618bffc59873cbd3692311d5875e61bdfbbb07ca
Jonny Irwin <M8R-drk9k41@mailinator.com>
2020-04-04 10:16:52 +0100

    some combinators - only in es6 module format currently

---
+++ combinators/index.js
@@ -0,0 +1,25 @@
+const identity, I;
+Identity = I = a => a;
+
+const tap, K;
+tap = K = fn => a => a;
+
+const alternation, OR;
+alternation = OR = (fnX, fnY) => val => fnX(val) || fnY(val);
+
+const sequence, S;
+sequence = S = (...xs) => val => xs.forEach(x => x(val));
+
+const fork;
+fork = (join, fnX, fnY) => val => join(fnX(val), fnY(val));
+
+export {
+	identity,
+	I,
+	alternation,
+	OR,
+	sequence,
+}
+	S,
+	fork
+}
